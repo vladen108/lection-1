3fe0d75e18f9c48795635742ffbc0be31e03b1af
vladen108 <vladen108@gmail.com>
2022-10-11 21:12:28 +0400

    Ex002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Text your name");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
